f89beba3c0522b63492575927ecb24ddcbe7f307
Ricardo Gonzalez <rickgc@gmail.com>
2025-03-17 11:55:46 +0000

    💣 Refactor

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,97 @@
 import { CallTemplateFunctionArgs, Context, PluginDefinition } from '@yaakapp/api';
-import { v1 as uuidv1, v4 as uuidv4, v6 as uuidv6, v7 as uuidv7 } from 'uuid';
+import { v1 as uuidv1, v3 as uuidv3, v4 as uuidv4, v5 as uuidv5, v6 as uuidv6, v7 as uuidv7 } from 'uuid';
 
-const algorithms = ['v1', 'v4', 'v6', 'v7'];
 
 export const plugin: PluginDefinition = {
-    templateFunctions: algorithms.map(algorithm => ({
-        name: `uuid.${algorithm}`,
-        description: `Generates a ${algorithm} UUID`,
-        args: [],
-        async onRender(_ctx: Context, args: CallTemplateFunctionArgs): Promise<string | null> {
-            let uuid;
-            switch (algorithm) {
-                case 'v1':
-                    uuid = uuidv1();
-                    break;
-                case 'v4':
-                    uuid = uuidv4();
-                    break;
-                case 'v6':
-                    uuid = uuidv6();
-                    break;
-                case 'v7':
-                    uuid = uuidv7();
-                    break;
-                default:
-                    return null;
-            }
-            return uuid;
-
+    templateFunctions: [
+        {
+            name: "uuid.v1",
+            description: "Generates a v1 UUID",
+            args: [],
+            async onRender(_ctx: Context, args: CallTemplateFunctionArgs): Promise<string | null> {
+                return uuidv1();
+            },
         },
-    })),
+        {
+            name: "uuid.v3",
+            description: "Generates a v3 UUID",
+            args: [
+                {
+                    name: "name",
+                    label: "Name",
+                    type: "text",
+                    optional: false,
+                },
+                {
+                    name: "namespace",
+                    label: "Namespace",
+                    type: "text",
+                    defaultValue: uuidv1(),
+                    optional: false,
+
+                },
+            ],
+            async onRender(_ctx: Context, { values }: CallTemplateFunctionArgs): Promise<string | null> {
+                if (!values.name || !values.namespace) {
+                    console.error('Missing required arguments');
+                    return null;
+                };
+
+                return uuidv3(values.name, values.namespace);
+            },
+        },
+        {
+            name: "uuid.v4",
+            description: "Generates a v4 UUID",
+            args: [],
+            async onRender(_ctx: Context, args: CallTemplateFunctionArgs): Promise<string | null> {
+                return uuidv4();
+            },
+        },
+        {
+            name: "uuid.v5",
+            description: "Generates a v5 UUID",
+            args: [
+                {
+                    name: "name",
+                    label: "Name",
+                    type: "text",
+                    optional: false,
+                },
+                {
+                    name: "namespace",
+                    label: "Namespace",
+                    type: "text",
+                    defaultValue: uuidv1(),
+                    optional: false,
+
+                },
+            ],
+            async onRender(_ctx: Context, { values }: CallTemplateFunctionArgs): Promise<string | null> {
+                if (!values.name || !values.namespace) {
+                    console.error('Missing required arguments');
+                    return null;
+                };
+
+                return uuidv5(values.name, values.namespace);
+            },
+        },
+        {
+            name: "uuid.v6",
+            description: "Generates a v6 UUID",
+            args: [],
+            async onRender(_ctx: Context, args: CallTemplateFunctionArgs): Promise<string | null> {
+                return uuidv6();
+            },
+        },
+        {
+            name: "uuid.v7",
+            description: "Generates a v7 UUID",
+            args: [],
+            async onRender(_ctx: Context, args: CallTemplateFunctionArgs): Promise<string | null> {
+                return uuidv7();
+            },
+        },
+
+    ],
 };
